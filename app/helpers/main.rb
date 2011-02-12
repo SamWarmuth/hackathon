7d766882960f77bcddbf4feb88cb50d28ad3603e
Sam Warmuth <sam@samwarmuth.com>
2011-02-12 16:34:13 -0600

    remove caching code

--- app/helpers/main.rb
+++ app/helpers/main.rb
@@ -2,11 +2,7 @@ class Main
   helpers do
     def logged_in?
       return false unless request.cookies.has_key?("user_challenge") && request.cookies.has_key?("user")
-      user = $cached_users[request.cookies['user']]
-      if user.nil?
-        user = User.get(request.cookies['user'])
-        $cached_users[user.id] = user unless user.nil?
-      end
+      user = User.get(request.cookies['user'])
       
       return false if user.nil?
       return false unless user.challenges && user.challenges.include?(request.cookies['user_challenge'])
