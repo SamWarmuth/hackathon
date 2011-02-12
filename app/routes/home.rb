50707bbe18b03c3b7b589b7279c5acd5581957ab
Bob Ralian <bob.ralian@gmail.com>
2011-02-12 15:49:03 -0600

    here are all of the pages

--- app/routes/home.rb
+++ app/routes/home.rb
@@ -2,4 +2,19 @@ class Main
   get "/" do
     erb :index
   end
+  get "/place" do
+    erb :place
+  end
+  get "/rate-it" do
+    erb :rateit
+  end
+  get "/best-stuff" do
+    erb :beststuff
+  end
+  get "/how-it-works" do
+    erb :howitworks
+  end
+  get "/terms-and-privacy" do
+    erb :termsandprivacy
+  end
 end
